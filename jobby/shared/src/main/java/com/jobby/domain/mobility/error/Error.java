0fee9f02a35eb2726a7345dbe2d694f72036e628
Amapola <htrsanbl@gmail.com>
2026-04-04 19:36:25 -0500

    shared/src/main/java/com/jobby/domain/mobility/result

--- jobby/shared/src/main/java/com/jobby/domain/mobility/error/Error.java
+++ jobby/shared/src/main/java/com/jobby/domain/mobility/error/Error.java
@@ -3,10 +3,12 @@ package com.jobby.domain.mobility.error;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @EqualsAndHashCode
+@ToString
 public class Error {
     private ErrorType code;
     private Field[] fields;
--- jobby/shared/src/main/java/com/jobby/domain/mobility/error/Field.java
+++ jobby/shared/src/main/java/com/jobby/domain/mobility/error/Field.java
@@ -1,13 +1,11 @@
 package com.jobby.domain.mobility.error;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @AllArgsConstructor
 @EqualsAndHashCode
+@ToString
 public class Field {
     @Setter
     private String instance;
