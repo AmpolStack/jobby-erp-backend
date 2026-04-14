fd02e5bd33c0eed332b3d03000a561beaebbc530
Heyther <htrsanbl@gmail.com>
2026-04-14 08:22:50 -0500

    refactor(user-service): The latest change requires updating the mappers

--- jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/mappers/ContactTypeMapper.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/mappers/ContactTypeMapper.java
@@ -4,10 +4,14 @@ import com.jobby.userservice.domain.models.ContactType;
 import com.jobby.userservice.infrastructure.persistence.entities.MongoContactTypeEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ContactTypeMapper {
 
     ContactType toDomain(MongoContactTypeEntity entity);
+
+    List<ContactType> toDomain(List<MongoContactTypeEntity> entity);
 
     MongoContactTypeEntity toEntity(ContactType domain);
 }
--- jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/mappers/IdentificationTypeMapper.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/mappers/IdentificationTypeMapper.java
@@ -4,10 +4,14 @@ import com.jobby.userservice.domain.models.IdentificationType;
 import com.jobby.userservice.infrastructure.persistence.entities.MongoIdentificationTypeEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface IdentificationTypeMapper {
 
     IdentificationType toDomain(MongoIdentificationTypeEntity entity);
+
+    List<IdentificationType> toDomain(List<MongoIdentificationTypeEntity> entity);
 
     MongoIdentificationTypeEntity toEntity(IdentificationType domain);
 }
