5060529293a72cea4700064afc30be1073873299
Heyther <htrsanbl@gmail.com>
2026-04-21 01:45:39 -0500

    feat(user-service.application): user field are removed in commands

--- jobby/user-service/src/main/java/com/jobby/userservice/application/commands/CreateOwnerCommand.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/application/commands/CreateOwnerCommand.java
@@ -1,6 +1,5 @@
 package com.jobby.userservice.application.commands;
 
-import com.jobby.userservice.domain.models.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
