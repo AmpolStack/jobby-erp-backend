e0629ad2e5455faf162e9eef3598322f1c54fad1
Heyther <htrsanbl@gmail.com>
2026-04-21 01:23:06 -0500

    feat(shared): the dependency versions has been updated in encryption library

--- jobby/shared/src/main/java/com/jobby/infrastructure/adapter/encrypt/AESEncryptionService.java
+++ jobby/shared/src/main/java/com/jobby/infrastructure/adapter/encrypt/AESEncryptionService.java
@@ -1,141 +1,104 @@
 package com.jobby.infrastructure.adapter.encrypt;
 
-import com.jobby.domain.configurations.EncryptConfig;
+import com.jobby.infrastructure.configurations.EncryptConfig;
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.error.ErrorType;
 import com.jobby.domain.mobility.error.Field;
 import com.jobby.domain.mobility.result.Result;
-import com.jobby.domain.mobility.validator.ValidationChain;
-import com.jobby.domain.ports.SafeResultValidator;
 import com.jobby.domain.ports.security.encrypt.EncryptBuilder;
 import com.jobby.domain.ports.security.encrypt.EncryptionService;
+import com.jobby.infrastructure.adapter.CryptoUtils;
+import lombok.AllArgsConstructor;
 import javax.crypto.Cipher;
 import javax.crypto.spec.GCMParameterSpec;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.util.Arrays;
-import java.util.Base64;
 
+@AllArgsConstructor
 public class AESEncryptionService implements EncryptionService {
+
     private static final String ALGORITHM = "AES";
     private static final String TRANSFORMATION = "AES/GCM/NoPadding";
-    private static final Integer[] VALID_T_LENGTHS_BITS = {98, 112, 120, 128};
-    private static final Integer[] VALID_KEY_LENGTHS_BITS = {128, 192, 256};
 
-    private final SafeResultValidator validator;
     private final EncryptBuilder encryptBuilder;
+    private final EncryptConfig config;
 
-    public AESEncryptionService(SafeResultValidator validator, EncryptBuilder defaultEncryptBuilder) {
-        this.validator = validator;
-        this.encryptBuilder = defaultEncryptBuilder;
+    @Override
+    public Result<String, Error> encryptAsBase64(String data) {
+        return encryptAsBytes(data)
+                .map(CryptoUtils::encodeBase64);
     }
 
     @Override
-    public Result<String, Error> decrypt(String cipherText, EncryptConfig config) {
-        return  validateConfig(config)
-                .flatMap(v -> validateAndParseCipherText(cipherText))
-                .flatMap(combined -> ValidationChain.create()
-                        .validateInternalGreaterThan(
-                                combined.length, config.getIv().getLength(),
-                                "combined")
-                        .build()
-                        .flatMap(v -> validateAndParseKey(config.getSecretKey()))
-                        .flatMap( key -> {
-                            var rawIv = Arrays.copyOfRange(combined, 0, config.getIv().getLength());
-                            var data = Arrays.copyOfRange(combined, config.getIv().getLength(), combined.length);
-                            var iv = new GCMParameterSpec(config.getIv().getTLen(), rawIv);
-                            return this.encryptBuilder
-                                    .setData(data)
-                                    .setIv(iv)
-                                    .setKey(key)
-                                    .setMode(Cipher.DECRYPT_MODE)
-                                    .setTransformation(TRANSFORMATION)
-                                    .build()
-                                    .map(bytesResp -> new String(bytesResp, StandardCharsets.UTF_8));
-                        })
-                );
-    }
+    public Result<byte[], Error> encryptAsBytes(String data) {
+        if (data == null || data.isBlank()) {
+            return Result.success(new byte[0]);
+        }
 
-    @Override
-    public Result<String, Error> encrypt(String data, EncryptConfig config) {
-        return validateConfig(config)
-                .flatMap(x -> validateAndParseKey(config.getSecretKey()))
-                .flatMap((key -> {
-                    var iv = EncryptUtils.generateIv(config.getIv().getLength(), config.getIv().getTLen());
-                    return this.encryptBuilder
+        return CryptoUtils.validateAndParseKey(
+                        ALGORITHM,
+                        config.getSecretKey(),
+                        EncryptionCryptography.VALID_KEY_LENGTHS_BITS)
+                .flatMap(key -> {
+                    var iv = EncryptionCryptography.generateIv(
+                            config.getIv().getLength(),
+                            config.getIv().getTLen());
+
+                    return encryptBuilder
                             .setData(data.getBytes(StandardCharsets.UTF_8))
                             .setIv(iv)
                             .setKey(key)
                             .setMode(Cipher.ENCRYPT_MODE)
                             .setTransformation(TRANSFORMATION)
                             .build()
-                            .map(cipherBytes ->{
+                            .map(cipherBytes -> {
                                 var buffer = ByteBuffer.allocate(iv.getIV().length + cipherBytes.length);
                                 buffer.put(iv.getIV());
                                 buffer.put(cipherBytes);
-                                var combined = buffer.array();
-                                return Base64.getEncoder().encodeToString(combined);
+                                return buffer.array();
                             });
-                }));
-    }
-
-
-    private Result<Void, Error> validateConfig(EncryptConfig config) {
-        // TODO: update when VALIDATION CHAIN is upgraded
-        return ValidationChain.create()
-                .validateInternalNotNull(config, "encrypt config")
-                .build()
-                .flatMap(x -> ValidationChain.create()
-                .add(this.validator.validate(config))
-                .validateInternalAnyMatch(
-                        config.getIv().getTLen(),
-                        VALID_T_LENGTHS_BITS,
-                        "t-len")
-                .build());
-    }
-
-    private Result<Key, Error> validateAndParseKey(String keyBase64){
-        final int BIT_MULTIPLIER = 8;
-        return ValidationChain.create()
-                .validateInternalNotBlank(keyBase64, "key-base-64")
-                .build()
-                .flatMap(v -> {
-                    var key = EncryptUtils.ParseKeySpec(ALGORITHM, keyBase64);
-
-                    if(key == null){
-                        return Result.failure(ErrorType.ITS_SERIALIZATION_ERROR,
-                                new Field("key-base-64", "is invalid in base64"));
-                    }
-
-                    var keyLengthInBytes = key.getEncoded().length * BIT_MULTIPLIER;
-
-                    return ValidationChain.create()
-                            .validateInternalAnyMatch(keyLengthInBytes, VALID_KEY_LENGTHS_BITS, "key-base-64-bytes")
-                            .build()
-                            .map(v3 -> key);
                 });
     }
 
-    private Result<byte[], Error> validateAndParseCipherText(String cipherText){
-        return ValidationChain.create()
-                .validateInternalNotBlank(cipherText, "cipher-text")
-                .build()
-                .flatMap(v -> {
-                    byte[] combined;
-                    try{
-                        combined = Base64.getDecoder().decode(cipherText);
-                    }
-                    catch (IllegalArgumentException e){
-                        return Result.failure(ErrorType.ITS_SERIALIZATION_ERROR,
-                                new Field(
-                                        "cipherText",
-                                        "Invalid Base64 encoded cipher text"
-                                )
-                        );
-                    }
-                    return Result.success(combined);
-                });
+    @Override
+    public Result<String, Error> decryptFromBase64(String cipherText) {
+        return CryptoUtils.decodeBase64(cipherText, "cipher-text")
+                .flatMap(this::decryptFromBytes);
     }
 
+    @Override
+    public Result<String, Error> decryptFromBytes(byte[] combined) {
+        if (combined == null || combined.length == 0) {
+            return Result.success(null);
+        }
+
+        int ivLength = config.getIv().getLength();
+
+        if (combined.length <= ivLength) {
+            return Result.failure(
+                    ErrorType.ITS_INVALID_OPTION_PARAMETER,
+                    new Field("cipher-data", "Data too short to contain IV")
+            );
+        }
+
+        return CryptoUtils.validateAndParseKey(
+                ALGORITHM,
+                config.getSecretKey(),
+                EncryptionCryptography.VALID_KEY_LENGTHS_BITS)
+                .flatMap(key -> {
+                        var rawIv = Arrays.copyOfRange(combined, 0, config.getIv().getLength());
+                        var data = Arrays.copyOfRange(combined, config.getIv().getLength(), combined.length);
+                        var iv = new GCMParameterSpec(config.getIv().getTLen(), rawIv);
+
+                        return encryptBuilder
+                                .setData(data)
+                                .setIv(iv)
+                                .setKey(key)
+                                .setMode(Cipher.DECRYPT_MODE)
+                                .setTransformation(TRANSFORMATION)
+                                .build()
+                                .map(bytes -> new String(bytes, StandardCharsets.UTF_8));
+                });
+    }
 }
