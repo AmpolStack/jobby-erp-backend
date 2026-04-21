ada18bf453269625ac6386525837015de60f2e19
Heyther <htrsanbl@gmail.com>
2026-04-21 01:20:32 -0500

    refactor: Base64 support has been added to the encryption and mac service

--- jobby/shared/src/main/java/com/jobby/domain/ports/security/hashing/mac/MacService.java
+++ jobby/shared/src/main/java/com/jobby/domain/ports/security/hashing/mac/MacService.java
@@ -1,10 +1,9 @@
 package com.jobby.domain.ports.security.hashing.mac;
 
-import com.jobby.domain.configurations.MacConfig;
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.result.Result;
 
 public interface MacService {
-    Result<byte[], Error> generateMac(String data, MacConfig config);
-    Result<Boolean, Error> verifyMac(String data, byte[] mac, MacConfig config);
+    Result<byte[], Error> generateMac(String data);
+    Result<Boolean, Error> verifyMac(String data, byte[] mac);
 }
