f7a07afff3a040c6daf3aeca1fe56fd515f9e1b9
Heyther <htrsanbl@gmail.com>
2026-04-21 01:29:33 -0500

    feat(shared): Security Policy enums are added to handle the security strategy behavior.

---
+++ jobby/shared/src/main/java/com/jobby/infrastructure/security/policies/FetchSecurityPolicy.java
@@ -0,0 +1,6 @@
+package com.jobby.infrastructure.security.policies;
+
+public enum FetchSecurityPolicy {
+    UNSEALED,
+    SEALED_CIPHER_REFERENCED,
+}
---
+++ jobby/shared/src/main/java/com/jobby/infrastructure/security/policies/StorageSecurityPolicy.java
@@ -0,0 +1,7 @@
+package com.jobby.infrastructure.security.policies;
+
+public enum StorageSecurityPolicy {
+    SECURED_ONLY_ENCRYPTION,
+    SECURED_ONLY_HASHING,
+    SECURED
+}
