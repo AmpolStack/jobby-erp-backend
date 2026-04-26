8787e118c7d0fe4d6d1a00852d39f798fe91b04a
Heyther <htrsanbl@gmail.com>
2026-04-25 22:56:47 -0500

    test: add nullity ops

---
+++ jobby/user-service/src/test/java/com/jobby/userservice/NullityOps.java
@@ -0,0 +1,27 @@
+package com.jobby.userservice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class NullityOps {
+
+    public static final List<String> BLANK_VALUES = Arrays.asList("", "    ", "\t", "\n");
+
+    public static String getNullityName(String value) {
+        return switch (value){
+            case null -> "null";
+            case "" -> "empty string ''";
+            case "    " -> "spaces '   '";
+            case "\t" -> "tab '/t'";
+            case "\n" -> "newline '/n'";
+            default -> "other";
+        };
+    }
+
+    public static String getNullityName(Object object){
+        if(object instanceof String){
+            return getNullityName(object);
+        }
+        return "null";
+    }
+}
