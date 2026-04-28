baf529ac6a2d4a49b17d4a777a6215071a4bc45b
Heyther <htrsanbl@gmail.com>
2026-04-28 17:06:05 -0500

    refactor(user-service.application): the use case was updated

--- jobby/user-service/src/main/java/com/jobby/userservice/application/useCases/CreateOwnerUseCase.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/application/useCases/CreateOwnerUseCase.java
@@ -1,18 +1,24 @@
 package com.jobby.userservice.application.useCases;
 
+import cn.hutool.core.lang.Pair;
+import com.jobby.domain.functional.PersistenceTask;
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.error.ErrorType;
 import com.jobby.domain.mobility.error.Field;
 import com.jobby.domain.mobility.result.Result;
 import com.jobby.domain.ports.IdGenerator;
+import com.jobby.domain.ports.TransactionOrchestrator;
 import com.jobby.userservice.application.commands.CreateOwnerCommand;
 import com.jobby.userservice.application.commands.CreateUserCommand;
 import com.jobby.userservice.application.mapper.CreateOwnerCommandMapper;
 import com.jobby.userservice.application.mapper.CreateUserCommandMapper;
 import com.jobby.userservice.application.mapper.GetOwnerQueryMapper;
 import com.jobby.userservice.application.queries.GetOwnerQuery;
+import com.jobby.userservice.domain.models.Owner;
+import com.jobby.userservice.domain.models.User;
 import com.jobby.userservice.domain.ports.IdentificationTypeRepository;
 import com.jobby.userservice.domain.ports.OwnerRepository;
+import com.jobby.userservice.domain.ports.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,34 +27,59 @@ import org.springframework.stereotype.Service;
 public class CreateOwnerUseCase {
 
     private final OwnerRepository ownerRepository;
+    private final UserRepository userRepository;
     private final IdentificationTypeRepository identificationTypeRepository;
     private final IdGenerator idGenerator;
-    private final GetOwnerQueryMapper ownerQueryMapper;
+    private final GetOwnerQueryMapper queryMapper;
+    private final TransactionOrchestrator transaction;
     private final CreateUserCommandMapper createUserCommandMapper;
     private final CreateOwnerCommandMapper createOwnerCommandMapper;
 
     public Result<GetOwnerQuery, Error> execute(CreateOwnerCommand command) {
         var userCmd = command.getUser();
         return validateUniqueness(userCmd)
-                .flatMap(v -> identificationTypeRepository.findById(userCmd.getIdentificationTypeId()))
-                .flatMap(identificationType -> idGenerator.next()
-                        .flatMap(userId -> createUserCommandMapper.toUser(userCmd, userId, "owner", identificationType)))
-                .flatMap(user -> idGenerator.next()
-                        .flatMap(ownerId -> createOwnerCommandMapper.toOwner(command, ownerId, user.getId()))
-                        .flatMap(owner -> ownerRepository.save(owner, user)
-                                .map(v -> ownerQueryMapper.toGetOwnerQuery(owner, user))));
+                .flatMap(v -> prepareUser(command.getUser()))
+                .flatMap((userTuple) ->
+                        prepareOwner(command, userTuple.getValue().getId())
+                        .flatMap(ownerTuple ->
+                                this.transaction.write()
+                                .add(userTuple.getKey())
+                                .add(ownerTuple.getKey())
+                                .build()
+                                .map(v ->
+                                        this.queryMapper.toGetOwnerQuery(ownerTuple.getValue(),
+                                                userTuple.getValue()))
+                        )
+                );
     }
 
     private Result<Void, Error> validateUniqueness(CreateUserCommand userCmd) {
-        return ownerRepository.existByEmail(userCmd.getEmail())
+        return this.userRepository.existByEmail(userCmd.getEmail())
                 .flatMap(exist -> exist
                         ? Result.failure(ErrorType.VALIDATION_ERROR, new Field("user", "A user with that email address already exists."))
-                        : ownerRepository.existByPhone(userCmd.getPhone()))
+                        : this.userRepository.existByPhone(userCmd.getPhone()))
                 .flatMap(exist -> exist
                         ? Result.failure(ErrorType.VALIDATION_ERROR, new Field("user", "There is already a registered user with that phone number."))
-                        : ownerRepository.existByIdentificationNumber(userCmd.getIdentificationNumber()))
+                        : this.userRepository.existByIdentificationNumber(userCmd.getIdentificationNumber()))
                 .flatMap(exist -> exist
                         ? Result.failure(ErrorType.VALIDATION_ERROR, new Field("user", "A user with that identification number is already registered."))
                         : Result.success());
+    }
+
+    private Result<Pair<PersistenceTask, User>,Error> prepareUser(CreateUserCommand cmd){
+        return this.identificationTypeRepository.findById(cmd.getIdentificationTypeId())
+                .flatMap(identificationType -> this.idGenerator.next()
+                        .flatMap(userId ->
+                                this.createUserCommandMapper.toUser(cmd, userId, "owner", identificationType))
+                .flatMap(user -> this.userRepository.prepareSave(user)
+                        .map(task -> Pair.of(task, user)))
+                );
+    }
+
+    private Result<Pair<PersistenceTask, Owner>,Error> prepareOwner(CreateOwnerCommand cmd, long userId){
+        return this.idGenerator.next()
+                .flatMap(ownerId -> createOwnerCommandMapper.toOwner(cmd, ownerId, userId))
+                .flatMap(owner -> this.ownerRepository.prepareSave(owner)
+                        .map(task -> Pair.of(task, owner)));
     }
 }
