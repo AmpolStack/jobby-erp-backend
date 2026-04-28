b603b6c5449a5d25165920c6b59352da723bdf61
Heyther <htrsanbl@gmail.com>
2026-04-28 17:01:05 -0500

    feat: add transactionOrchestrator

--- jobby/shared/src/main/java/com/jobby/infrastructure/autoconfiguration/TransactionAutoConfiguration.java
+++ jobby/shared/src/main/java/com/jobby/infrastructure/autoconfiguration/TransactionAutoConfiguration.java
@@ -1,22 +1,33 @@
 package com.jobby.infrastructure.autoconfiguration;
 
-import com.jobby.infrastructure.transaction.TransactionExecutor;
-import com.jobby.infrastructure.transaction.TransactionalContext;
+import com.jobby.infrastructure.transaction.TransactionOrchestratorAdapter;
+import com.jobby.infrastructure.transaction.SpringDataTransactionalContext;
+import com.jobby.infrastructure.transaction.proxy.MongoDbSpringDataHandler;
+import com.jobby.infrastructure.transaction.proxy.PersistenceProxy;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TransactionAutoConfiguration {
+
     @Bean
     @ConditionalOnMissingBean
-    public TransactionalContext persistenceTransactionContext() {
-        return new TransactionalContext();
+    public SpringDataTransactionalContext persistenceTransactionContext() {
+        return new SpringDataTransactionalContext();
+    }
+
+    @Bean("defaultPersistenceProxy")
+    @ConditionalOnMissingBean
+    public PersistenceProxy defaultPersistenceProxy() {
+        return new MongoDbSpringDataHandler();
     }
 
     @Bean
     @ConditionalOnMissingBean
-    public TransactionExecutor transactionExecutor(TransactionalContext context){
-        return new TransactionExecutor(context);
+    public TransactionOrchestratorAdapter transactionExecutor(
+            SpringDataTransactionalContext context,
+            PersistenceProxy persistenceProxy) {
+        return new TransactionOrchestratorAdapter(context, persistenceProxy);
     }
 }
