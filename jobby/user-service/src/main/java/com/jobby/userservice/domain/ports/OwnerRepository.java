2bfec907170c1d524160f11e58bd54efa224e7cd
Heyther <htrsanbl@gmail.com>
2026-04-28 17:02:29 -0500

    refactor(user-service.domain.ports): The owner repository nows defines the required ' prepare and commit pattern ' in implementation

--- jobby/user-service/src/main/java/com/jobby/userservice/domain/ports/OwnerRepository.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/domain/ports/OwnerRepository.java
@@ -1,13 +1,10 @@
 package com.jobby.userservice.domain.ports;
 
+import com.jobby.domain.functional.PersistenceTask;
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.result.Result;
 import com.jobby.userservice.domain.models.Owner;
-import com.jobby.userservice.domain.models.User;
 
 public interface OwnerRepository {
-    Result<Void, Error> save(Owner owner, User user);
-    Result<Boolean, Error> existByEmail(String email);
-    Result<Boolean, Error> existByPhone(String email);
-    Result<Boolean, Error> existByIdentificationNumber(String identificationNumber);
+    Result<PersistenceTask, Error> prepareSave(Owner owner);
 }
--- jobby/user-service/src/main/java/com/jobby/userservice/domain/ports/UserRepository.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/domain/ports/UserRepository.java
@@ -1,14 +1,13 @@
 package com.jobby.userservice.domain.ports;
 
+import com.jobby.domain.functional.PersistenceTask;
+import com.jobby.domain.mobility.error.Error;
+import com.jobby.domain.mobility.result.Result;
 import com.jobby.userservice.domain.models.User;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface UserRepository {
-    Optional<User> findById(Long id);
-    List<User> findAll();
-    User save(User user);
-    void deleteById(Long id);
-    boolean existsById(Long id);
+    Result<PersistenceTask, Error> prepareSave(User user);
+    Result<Boolean, Error> existByEmail(String email);
+    Result<Boolean, Error> existByPhone(String email);
+    Result<Boolean, Error> existByIdentificationNumber(String identificationNumber);
 }
