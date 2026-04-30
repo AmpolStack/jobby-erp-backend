f7c95569d9b99784cdfaf9768aa679d70dd82a00
Heyther <htrsanbl@gmail.com>
2026-04-29 23:55:44 -0500

    refactor: The index and protected fields were updated, now uses the lombok.NoArgsContructor for compatibility with mongo-spring-data-entity-building

--- jobby/shared/src/main/java/com/jobby/infrastructure/security/fields/ProtectedField.java
+++ jobby/shared/src/main/java/com/jobby/infrastructure/security/fields/ProtectedField.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Transient;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class ProtectedField {
