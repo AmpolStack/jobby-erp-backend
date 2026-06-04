14124af91a3c891711aa349aed75969985565620
Heyther <htrsanbl@gmail.com>
2026-06-04 02:54:21 -0500

    refactor: update RemoveRecoveryEmailUseCaseAdapter implementation

--- jobby/user-service/src/main/java/com/jobby/userservice/application/useCases/RemoveRecoveryEmailUseCaseAdapter.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/application/useCases/RemoveRecoveryEmailUseCaseAdapter.java
@@ -3,8 +3,11 @@ package com.jobby.userservice.application.useCases;
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.result.Result;
 import com.jobby.domain.ports.TransactionOrchestrator;
+import com.jobby.userservice.application.mappers.ResponseMapper;
+import com.jobby.userservice.domain.contract.commands.RemoveRecoveryEmailCommand;
 import com.jobby.userservice.domain.contract.responses.OwnerResponse;
 import com.jobby.userservice.domain.models.aggregate.Owner;
+import com.jobby.userservice.domain.ports.in.RemoveRecoveryEmailUseCase;
 import com.jobby.userservice.domain.ports.out.repositories.OwnerRepository;
 import com.jobby.userservice.domain.ports.out.repositories.UserRepository;
 import lombok.AllArgsConstructor;
@@ -12,22 +15,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class RemoveRecoveryEmailUseCase implements com.jobby.userservice.domain.ports.in.RemoveRecoveryEmailUseCase {
+public class RemoveRecoveryEmailUseCaseAdapter implements RemoveRecoveryEmailUseCase {
 
     private final OwnerRepository ownerRepository;
     private final UserRepository userRepository;
     private final TransactionOrchestrator transaction;
-    private final GetOwnerQueryMapper mapper;
+    private final ResponseMapper responseMapper;
 
-    public Result<OwnerResponse, Error> execute(long id){
-        return this.ownerRepository.getById(id)
+    public Result<OwnerResponse, Error> execute(RemoveRecoveryEmailCommand command){
+        return this.ownerRepository.getById(command.ownerId())
                 .peek(Owner::removeRecoveryEmail)
                 .flatMap(owner -> this.ownerRepository.prepareSave(owner)
                         .flatMap(task -> this.transaction.write()
                                 .add(task)
                                 .build())
                         .flatMap(v -> this.userRepository.getById(owner.getUserId()))
-                        .map(user -> this.mapper.toGetOwnerQuery(owner, user))
+                        .map(user -> this.responseMapper.toResponse(owner, user))
                 );
     }
 }
