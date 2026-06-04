1b4facf0d93ac431089f37965e65984c16aaeb99
Heyther <htrsanbl@gmail.com>
2026-06-04 02:54:17 -0500

    refactor: update RemoveProfileImageUseCaseAdapter implementation

--- jobby/user-service/src/main/java/com/jobby/userservice/application/useCases/RemoveProfileImageUseCaseAdapter.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/application/useCases/RemoveProfileImageUseCaseAdapter.java
@@ -2,31 +2,32 @@ package com.jobby.userservice.application.useCases;
 
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.result.Result;
-import com.jobby.domain.ports.CacheService;
 import com.jobby.domain.ports.TransactionOrchestrator;
+import com.jobby.userservice.application.mappers.ResponseMapper;
+import com.jobby.userservice.domain.contract.commands.RemoveProfileImageCommand;
 import com.jobby.userservice.domain.contract.responses.UserResponse;
-import com.jobby.userservice.domain.ports.out.services.FileStorageService;
+import com.jobby.userservice.domain.ports.in.RemoveProfileImageUseCase;
+import com.jobby.userservice.domain.ports.out.services.StorageService;
 import com.jobby.userservice.domain.ports.out.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class RemoveProfileImageUseCase {
-    private final FileStorageService profileImageRepository;
-    private final GetUserQueryMapper getUserQueryMapper;
+public class RemoveProfileImageUseCaseAdapter implements RemoveProfileImageUseCase {
+    private final StorageService fileStorageService;
+    private final ResponseMapper responseMapper;
     private final UserRepository userRepository;
     private final TransactionOrchestrator transaction;
-    private final CacheService cacheService;
 
-    public Result<UserResponse, Error> execute(long userId){
-        return this.userRepository.getById(userId)
+    public Result<UserResponse, Error> execute(RemoveProfileImageCommand command){
+        return this.userRepository.getById(command.userId())
                 .flatMap(user -> user.removeProfileImage()
-                        .flatMap(v -> this.profileImageRepository.remove(user.getProfileImageUrl()))
+                        .flatMap(v -> this.fileStorageService.removeProfileImage(user.getProfileImageUrl()))
                         .flatMap(v -> this.userRepository.prepareSave(user))
                         .flatMap(userTask -> this.transaction.write()
                                 .add(userTask)
                                 .build())
-                        .map(v -> this.getUserQueryMapper.toGetUserQuery(user)));
+                        .map(v -> this.responseMapper.toResponse(user)));
     }
 }
