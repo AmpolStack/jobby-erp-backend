8f13aaf4e5f67426459ba73ed6dc7005ac203d82
Heyther <htrsanbl@gmail.com>
2026-06-04 02:56:40 -0500

    refactor: update IdentificationTypeRepository port

--- jobby/user-service/src/main/java/com/jobby/userservice/domain/ports/out/repositories/IdentificationTypeRepository.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/domain/ports/out/repositories/IdentificationTypeRepository.java
@@ -1,8 +1,8 @@
-package com.jobby.userservice.domain.ports.out.repositories.models;
+package com.jobby.userservice.domain.ports.out.repositories;
 
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.result.Result;
-import com.jobby.userservice.domain.models.IdentificationType;
+import com.jobby.userservice.domain.models.reference.IdentificationType;
 import java.util.List;
 
 public interface IdentificationTypeRepository {
