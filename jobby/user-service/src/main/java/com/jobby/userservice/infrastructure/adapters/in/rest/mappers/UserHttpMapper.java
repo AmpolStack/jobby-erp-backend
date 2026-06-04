ce54c75befba15fb602363b0b3db589a16e48afe
Heyther <htrsanbl@gmail.com>
2026-06-04 02:57:39 -0500

    refactor: update UserHttpMapper

--- jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/adapters/in/rest/mappers/UserHttpMapper.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/adapters/in/rest/mappers/UserHttpMapper.java
@@ -1,10 +1,15 @@
-package com.jobby.userservice.infrastructure.adapters.in.mappers;
+package com.jobby.userservice.infrastructure.adapters.in.rest.mappers;
 
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.error.ErrorType;
 import com.jobby.domain.mobility.error.Field;
 import com.jobby.domain.mobility.result.Result;
-import com.jobby.userservice.domain.commands.UploadImageCommand;
+import com.jobby.userservice.domain.contract.commands.ConfirmEmailChangeCommand;
+import com.jobby.userservice.domain.contract.commands.RequestEmailChangeCommand;
+import com.jobby.userservice.domain.contract.commands.UpdateIProfileImageCommand;
+import com.jobby.userservice.infrastructure.adapters.in.rest.requests.ChangeEmailRequest;
+import com.jobby.userservice.infrastructure.adapters.in.rest.requests.ConfirmEmailRequest;
+import com.jobby.userservice.infrastructure.adapters.in.rest.responses.UserResponse;
 import org.mapstruct.Mapper;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,9 +17,9 @@ import java.io.IOException;
 
 @Mapper(componentModel = "spring")
 public interface UserHttpMapper {
-    default Result<UploadImageCommand, Error> toUploadImageCommand(long id, MultipartFile file){
+    default Result<UpdateIProfileImageCommand, Error> toCommand(long id, MultipartFile file){
         try {
-            var response = new UploadImageCommand(id,
+            var response = new UpdateIProfileImageCommand(id,
                     file.getOriginalFilename(),
                     file.getBytes(),
                     file.getSize(),
@@ -23,5 +28,15 @@ public interface UserHttpMapper {
         } catch (IOException e) {
             return Result.failure(ErrorType.INVALID_INPUT, new Field("file", "The file cannot be accessed"));
         }
+    }
+
+    default RequestEmailChangeCommand toCommand(ChangeEmailRequest request){
+        if(request == null) return null;
+        return new RequestEmailChangeCommand(request.userId(), request.email());
+    }
+
+    default ConfirmEmailChangeCommand toCommand(ConfirmEmailRequest request){
+        if(request == null) return null;
+        return new ConfirmEmailChangeCommand(request.userId(), request.code());
     }
 }
