13c9e487da50d861fa7ae68e80351565d183656e
Heyther <htrsanbl@gmail.com>
2026-06-04 02:54:14 -0500

    refactor: update GetOwnerByIdUseCaseAdapter implementation

--- jobby/user-service/src/main/java/com/jobby/userservice/application/useCases/GetOwnerByIdUseCaseAdapter.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/application/useCases/GetOwnerByIdUseCaseAdapter.java
@@ -2,7 +2,10 @@ package com.jobby.userservice.application.useCases;
 
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.result.Result;
+import com.jobby.userservice.application.mappers.ResponseMapper;
+import com.jobby.userservice.domain.contract.queries.GetOwnerByIdQuery;
 import com.jobby.userservice.domain.contract.responses.OwnerResponse;
+import com.jobby.userservice.domain.ports.in.GetOwnerByIdUseCase;
 import com.jobby.userservice.domain.ports.out.repositories.OwnerRepository;
 import com.jobby.userservice.domain.ports.out.repositories.UserRepository;
 import lombok.AllArgsConstructor;
@@ -10,16 +13,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class GetOwnerByIdUseCase {
+public class GetOwnerByIdUseCaseAdapter implements GetOwnerByIdUseCase {
 
     private final UserRepository userRepository;
     private final OwnerRepository ownerRepository;
-    private final GetOwnerQueryMapper mapper;
+    private final ResponseMapper responseMapper;
 
-    public Result<OwnerResponse, Error> execute(long id){
-        return this.ownerRepository.getById(id)
+    @Override
+    public Result<OwnerResponse, Error> execute(GetOwnerByIdQuery query){
+        return this.ownerRepository.getById(query.ownerId())
                 .flatMap(owner ->
                         this.userRepository.getById(owner.getUserId())
-                                .map(user -> this.mapper.toGetOwnerQuery(owner, user)));
+                                .map(user -> this.responseMapper.toResponse(owner, user)));
     }
 }
