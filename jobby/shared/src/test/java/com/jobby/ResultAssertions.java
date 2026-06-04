83b4c95e8c49df3dbb9e58a4d7aa18ee454a7eb5
Heyther <htrsanbl@gmail.com>
2026-06-04 02:53:09 -0500

    test: update ResultAssertions test utility

---
+++ jobby/shared/src/test/java/com/jobby/ResultAssertions.java
@@ -0,0 +1,31 @@
+package com.jobby;
+
+import com.jobby.domain.mobility.error.Error;
+import com.jobby.domain.mobility.error.ErrorType;
+import com.jobby.domain.mobility.result.Result;
+import org.junit.jupiter.api.Assertions;
+
+public class ResultAssertions {
+    public static <T,E> void assertSuccess(Result<T,E> result){
+        Assertions.assertTrue(result.isSuccess());
+    }
+
+    public static <T,E> void assertSuccess(Result<T,E> result, T expectedObject){
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertEquals(result.data(), expectedObject);
+    }
+
+    public static <T,E> void assertFailure(Result<T,E> obtained){
+        Assertions.assertTrue(obtained.isFailure());
+    }
+
+    public static <T> void assertFailure(Result<T,Error> obtained, ErrorType errorType){
+        Assertions.assertTrue(obtained.isFailure());
+        Assertions.assertSame(obtained.error().getCode(), errorType);
+    }
+
+    public static <T,T2> void assertFailure(Result<T, Error> obtained, Result<T2,Error> expected){
+        Assertions.assertTrue(obtained.isFailure());
+        Assertions.assertEquals(expected.error(), obtained.error());
+    }
+}
