aceec92aba3e9017eb0a4c48ba5df5a3fce0f699
Heyther <htrsanbl@gmail.com>
2026-06-04 02:58:47 -0500

    refactor: update MongoContactMapper

--- jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/persistence/mappers/entities/MongoContactMapper.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/persistence/mappers/entities/MongoContactMapper.java
@@ -1,8 +1,8 @@
 package com.jobby.userservice.infrastructure.persistence.mappers.entities;
 
 import com.jobby.infrastructure.security.fields.ProtectedField;
-import com.jobby.userservice.domain.models.Contact;
-import com.jobby.userservice.domain.vo.ContactValue;
+import com.jobby.userservice.domain.models.entity.Contact;
+import com.jobby.userservice.domain.models.vo.shared.ContactValue;
 import com.jobby.userservice.infrastructure.persistence.mappers.common.DomainVOMapper;
 import com.jobby.userservice.infrastructure.persistence.mappers.common.SecuredFieldMapper;
 import com.jobby.userservice.infrastructure.persistence.entities.MongoContactEntity;
