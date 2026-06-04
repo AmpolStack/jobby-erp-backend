9a8285b2356c3e353ff24a75ddba97a790e8e2a3
Heyther <htrsanbl@gmail.com>
2026-06-04 02:54:27 -0500

    refactor: update UpdateProfileImageUseCaseAdapter implementation

--- jobby/user-service/src/main/java/com/jobby/userservice/application/useCases/UpdateProfileImageUseCaseAdapter.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/application/useCases/UpdateProfileImageUseCaseAdapter.java
@@ -3,11 +3,13 @@ package com.jobby.userservice.application.useCases;
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.result.Result;
 import com.jobby.domain.ports.TransactionOrchestrator;
-import com.jobby.userservice.domain.contract.commands.UploadImageCommand;
+import com.jobby.userservice.application.mappers.ResponseMapper;
+import com.jobby.userservice.domain.contract.commands.UpdateIProfileImageCommand;
 import com.jobby.userservice.domain.contract.responses.UserResponse;
 import com.jobby.userservice.domain.models.aggregate.User;
+import com.jobby.userservice.domain.ports.in.UpdateProfileImageUseCase;
 import com.jobby.userservice.domain.ports.out.repositories.OwnerRepository;
-import com.jobby.userservice.domain.ports.out.services.FileStorageService;
+import com.jobby.userservice.domain.ports.out.services.StorageService;
 import com.jobby.userservice.domain.ports.out.repositories.UserRepository;
 import com.jobby.userservice.domain.models.vo.ephemeral.ImageStorageContext;
 import com.jobby.userservice.domain.models.vo.ephemeral.ProfileImage;
@@ -16,16 +18,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class UpdateProfileImageUseCase {
+public class UpdateProfileImageUseCaseAdapter implements UpdateProfileImageUseCase {
 
-    private final FileStorageService profileImageRepository;
+    private final StorageService fileStorageService;
     private final UserRepository userRepository;
     private final OwnerRepository ownerRepository;
 
-    private final GetUserQueryMapper getUserQueryMapper;
+    private final ResponseMapper responseMapper;
     private final TransactionOrchestrator transaction;
 
-    public Result<UserResponse, Error> execute(UploadImageCommand command){
+    public Result<UserResponse, Error> execute(UpdateIProfileImageCommand command){
         return this.userRepository.getById(command.userId())
                 .flatMap(user -> switch (user.getRole()){
                         case OWNER -> handleOwner(command, user);
@@ -33,7 +35,7 @@ public class UpdateProfileImageUseCase {
                     });
     }
 
-    private Result<UserResponse, Error> handleOwner(UploadImageCommand command, User user) {
+    private Result<UserResponse, Error> handleOwner(UpdateIProfileImageCommand command, User user) {
         return this.ownerRepository.getByUserId(user.getId())
                 .flatMap(owner -> {
                     var context = new ImageStorageContext.OwnerContext(owner.getId());
@@ -41,7 +43,7 @@ public class UpdateProfileImageUseCase {
                 });
     }
 
-    private Result<UserResponse, Error> handleEmployee(UploadImageCommand command, User user) {
+    private Result<UserResponse, Error> handleEmployee(UpdateIProfileImageCommand command, User user) {
         // TODO: Implements real logic
         var context = new ImageStorageContext.EmployeeContext(
         1, 1,1);
@@ -50,9 +52,9 @@ public class UpdateProfileImageUseCase {
 
 
     private Result<UserResponse, Error> uploadAndUpdate(
-            UploadImageCommand command, User user, ImageStorageContext context) {
+            UpdateIProfileImageCommand command, User user, ImageStorageContext context) {
         return ProfileImage.of(command.content(), command.contentType(), context)
-                .flatMap(profileImageRepository::changeProfileImage)
+                .flatMap(this.fileStorageService::changeProfileImage)
                 .flatMap(newImageUrl -> {
 
                     if(user.getProfileImageUrl() == null
@@ -61,13 +63,13 @@ public class UpdateProfileImageUseCase {
                     }
 
                     // TODO: Implement a strategy to avoid orphaned files without returning an error
-                    return this.profileImageRepository.remove(user.getProfileImageUrl())
+                    return this.fileStorageService.removeProfileImage(user.getProfileImageUrl())
                             .flatMap(v -> user.updateImageUrl(newImageUrl));
 
                 })
-                .flatMap(v -> userRepository.prepareSave(user))
-                .flatMap(task -> transaction.write().add(task).build())
-                .map(v -> getUserQueryMapper.toGetUserQuery(user));
+                .flatMap(v -> this.userRepository.prepareSave(user))
+                .flatMap(task -> this.transaction.write().add(task).build())
+                .map(v -> this.responseMapper.toResponse(user));
     }
 
 }
