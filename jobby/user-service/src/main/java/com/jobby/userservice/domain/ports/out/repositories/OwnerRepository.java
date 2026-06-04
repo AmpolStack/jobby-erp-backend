f598752fbad4cb4f6ed83d7c8d7065ce58b07489
Heyther <htrsanbl@gmail.com>
2026-06-04 02:56:46 -0500

    refactor: update OwnerRepository port

--- jobby/user-service/src/main/java/com/jobby/userservice/domain/ports/out/repositories/OwnerRepository.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/domain/ports/out/repositories/OwnerRepository.java
@@ -1,9 +1,9 @@
-package com.jobby.userservice.domain.ports.out.repositories.models;
+package com.jobby.userservice.domain.ports.out.repositories;
 
 import com.jobby.domain.functional.PersistenceTask;
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.result.Result;
-import com.jobby.userservice.domain.models.Owner;
+import com.jobby.userservice.domain.models.aggregate.Owner;
 
 public interface OwnerRepository {
     Result<PersistenceTask, Error> prepareSave(Owner owner);
