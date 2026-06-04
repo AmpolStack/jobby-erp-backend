b6bc78ad3b6febbfc4e3352e1c4877b8698bfe6e
Heyther <htrsanbl@gmail.com>
2026-06-04 02:57:14 -0500

    refactor: update ContactController

--- jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/adapters/in/rest/controllers/ContactController.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/adapters/in/rest/controllers/ContactController.java
@@ -1,4 +1,4 @@
-package com.jobby.userservice.infrastructure.adapters.in;
+package com.jobby.userservice.infrastructure.adapters.in.rest.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
