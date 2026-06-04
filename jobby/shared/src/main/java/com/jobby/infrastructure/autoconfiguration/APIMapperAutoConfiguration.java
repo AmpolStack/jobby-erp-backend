297ed5e6a71a6180c9541f8b08450493e2e16945
Heyther <htrsanbl@gmail.com>
2026-06-04 02:52:49 -0500

    refactor: update APIMapperAutoConfiguration

--- jobby/shared/src/main/java/com/jobby/infrastructure/autoconfiguration/APIMapperAutoConfiguration.java
+++ jobby/shared/src/main/java/com/jobby/infrastructure/autoconfiguration/APIMapperAutoConfiguration.java
@@ -1,16 +1,25 @@
 package com.jobby.infrastructure.autoconfiguration;
 
+import com.jobby.infrastructure.adapter.SupportIdProvider;
 import com.jobby.infrastructure.response.definition.HttpResponseProcessor;
 import com.jobby.infrastructure.response.implementation.problemdetails.ProblemDetailsResultMapper;
+import io.micrometer.tracing.Tracer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class APIMapperAutoConfiguration {
+
     @Bean
     @ConditionalOnMissingBean
-    public HttpResponseProcessor apiMapper(){
-        return new ProblemDetailsResultMapper();
+    public SupportIdProvider supportIdProvider(Tracer tracer){
+        return new SupportIdProvider(tracer);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public HttpResponseProcessor apiMapper(SupportIdProvider supportIdProvider){
+        return new ProblemDetailsResultMapper(supportIdProvider);
     }
 }
