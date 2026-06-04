cda49c79d6b3bfa40a1750d1bd7f98cc9b9a1a68
Heyther <htrsanbl@gmail.com>
2026-06-04 02:57:00 -0500

    refactor: update UserRepository port

--- jobby/user-service/src/main/java/com/jobby/userservice/domain/ports/out/repositories/UserRepository.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/domain/ports/out/repositories/UserRepository.java
@@ -1,9 +1,9 @@
-package com.jobby.userservice.domain.ports.out.repositories.models;
+package com.jobby.userservice.domain.ports.out.repositories;
 
 import com.jobby.domain.functional.PersistenceTask;
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.result.Result;
-import com.jobby.userservice.domain.models.User;
+import com.jobby.userservice.domain.models.aggregate.User;
 
 public interface UserRepository {
     Result<PersistenceTask, Error> prepareSave(User user);
