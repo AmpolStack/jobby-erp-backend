24d66e33022e9f518634d7d39856f05dd686f27b
Heyther <htrsanbl@gmail.com>
2026-06-04 02:57:11 -0500

    refactor: update StorageService port

--- jobby/user-service/src/main/java/com/jobby/userservice/domain/ports/out/services/StorageService.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/domain/ports/out/services/StorageService.java
@@ -5,7 +5,7 @@ import com.jobby.domain.mobility.result.Result;
 import com.jobby.userservice.domain.models.vo.shared.ImageUrl;
 import com.jobby.userservice.domain.models.vo.ephemeral.ProfileImage;
 
-public interface FileStorageService {
+public interface StorageService {
     Result<ImageUrl, Error> changeProfileImage(ProfileImage image);
     Result<Void, Error> removeProfileImage(ImageUrl url);
 }
