13af827ada1ff704896f09f63bc3827e7d4a227e
Heyther <htrsanbl@gmail.com>
2026-06-04 02:58:23 -0500

    refactor: update StorageServiceAdapter

--- jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/adapters/out/services/StorageServiceAdapter.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/adapters/out/services/StorageServiceAdapter.java
@@ -2,7 +2,7 @@ package com.jobby.userservice.infrastructure.adapters.out.services;
 
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.result.Result;
-import com.jobby.userservice.domain.ports.out.services.FileStorageService;
+import com.jobby.userservice.domain.ports.out.services.StorageService;
 import com.jobby.userservice.domain.models.vo.shared.ImageUrl;
 import com.jobby.userservice.domain.models.vo.ephemeral.ProfileImage;
 import lombok.AllArgsConstructor;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class FileStorageServiceAdapter implements FileStorageService {
+public class StorageServiceAdapter implements StorageService {
 
     private final com.jobby.infrastructure.adapter.FileStorageServiceAdapter fileStorageService;
 
