d23e1e9e660ec07427cb8c9759ca9d5f3e9e5169
Heyther <htrsanbl@gmail.com>
2026-06-05 23:54:36 -0500

    refactor: remove old response classes

--- jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/adapters/in/rest/responses/ContactResponse.java
+++
@@ -1,8 +0,0 @@
-package com.jobby.userservice.infrastructure.adapters.in.rest.responses;
-
-public record ContactResponse(
-        String name,
-        String description,
-        boolean isPublic,
-        String value
-) {}
--- jobby/user-service/src/main/java/com/jobby/userservice/infrastructure/adapters/in/rest/responses/OwnerResponse.java
+++
@@ -1,6 +0,0 @@
-package com.jobby.userservice.infrastructure.adapters.in.rest.responses;
-
-public record OwnerResponse(
-        UserResponse user,
-        String alternativeEmail
-) {}
