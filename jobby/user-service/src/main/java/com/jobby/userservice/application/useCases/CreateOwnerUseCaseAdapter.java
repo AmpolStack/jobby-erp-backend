c0349a8556fc051f7f6cb5d49dceec040f8c1a50
Heyther <htrsanbl@gmail.com>
2026-06-05 23:53:23 -0500

    refactor: update CreateOwnerUseCaseAdapter

--- jobby/user-service/src/main/java/com/jobby/userservice/application/useCases/CreateOwnerUseCaseAdapter.java
+++ jobby/user-service/src/main/java/com/jobby/userservice/application/useCases/CreateOwnerUseCaseAdapter.java
@@ -1,7 +1,5 @@
 package com.jobby.userservice.application.useCases;
 
-import cn.hutool.core.lang.Pair;
-import com.jobby.domain.functional.PersistenceTask;
 import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.error.ErrorType;
 import com.jobby.domain.mobility.error.Field;
@@ -15,11 +13,11 @@ import com.jobby.userservice.domain.contract.commands.CreateUserCommand;
 import com.jobby.userservice.domain.contract.responses.OwnerResponse;
 import com.jobby.userservice.domain.models.aggregate.Owner;
 import com.jobby.userservice.domain.models.aggregate.User;
+import com.jobby.userservice.domain.models.enums.Role;
 import com.jobby.userservice.domain.ports.in.CreateOwnerUseCase;
-import com.jobby.userservice.domain.ports.out.repositories.IdentificationTypeRepository;
 import com.jobby.userservice.domain.ports.out.repositories.OwnerRepository;
 import com.jobby.userservice.domain.ports.out.repositories.UserRepository;
-import com.jobby.userservice.domain.models.enums.Role;
+import com.jobby.userservice.domain.ports.out.services.ReferenceDataProvider;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -29,7 +27,7 @@ public class CreateOwnerUseCaseAdapter implements CreateOwnerUseCase {
 
     private final OwnerRepository ownerRepository;
     private final UserRepository userRepository;
-    private final IdentificationTypeRepository identificationTypeRepository;
+    private final ReferenceDataProvider referenceData;
     private final IdGenerator idGenerator;
     private final CommandMapper commandMapper;
     private final ResponseMapper responseMapper;
@@ -39,47 +37,44 @@ public class CreateOwnerUseCaseAdapter implements CreateOwnerUseCase {
         var userCmd = command.user();
         return validateUniqueness(userCmd)
                 .flatMap(v -> prepareUser(command.user()))
-                .flatMap((userTuple) ->
-                        prepareOwner(command, userTuple.getValue().getId())
-                        .flatMap(ownerTuple ->
-                                this.transaction.write()
-                                .add(userTuple.getKey())
-                                .add(ownerTuple.getKey())
-                                .build()
-                                .map(v ->
-                                        this.responseMapper.toResponse(ownerTuple.getValue(),
-                                                userTuple.getValue()))
-                        )
-                );
+                .flatMap(user -> prepareOwner(command, user.getId())
+                        .flatMap(owner -> executeTransaction(user, owner)));
     }
 
     private Result<Void, Error> validateUniqueness(CreateUserCommand userCmd) {
         return this.userRepository.existByEmail(userCmd.email())
                 .flatMap(exist -> exist
-                        ? Result.failure(ErrorType.VALIDATION_ERROR, new Field("user", "A user with that recoveryEmail address already exists."))
+                        ? Result.failure(ErrorType.VALIDATION_ERROR,
+                        new Field("user", "A user with that recoveryEmail address already exists."))
                         : this.userRepository.existByPhone(userCmd.phone()))
                 .flatMap(exist -> exist
-                        ? Result.failure(ErrorType.VALIDATION_ERROR, new Field("user", "There is already a registered user with that phone number."))
+                        ? Result.failure(ErrorType.VALIDATION_ERROR,
+                        new Field("user", "There is already a registered user with that phone number."))
                         : this.userRepository.existByIdentificationNumber(userCmd.identificationNumber()))
                 .flatMap(exist -> exist
-                        ? Result.failure(ErrorType.VALIDATION_ERROR, new Field("user", "A user with that identification number is already registered."))
+                        ? Result.failure(ErrorType.VALIDATION_ERROR,
+                        new Field("user", "A user with that identification number is already registered."))
                         : Result.success());
     }
 
-    private Result<Pair<PersistenceTask, User>,Error> prepareUser(CreateUserCommand cmd){
-        return this.identificationTypeRepository.findById(cmd.identificationTypeId())
+    private Result<User, Error> prepareUser(CreateUserCommand cmd) {
+        return this.referenceData.identificationType(cmd.identificationTypeId())
                 .flatMap(identificationType -> this.idGenerator.next()
-                        .flatMap(userId ->
-                                this.commandMapper.toUser(cmd, userId, Role.OWNER, identificationType))
-                .flatMap(user -> this.userRepository.prepareSave(user)
-                        .map(task -> Pair.of(task, user)))
-                );
+                        .flatMap(userId -> this.commandMapper.toUser(cmd, userId, Role.OWNER, identificationType)));
     }
 
-    private Result<Pair<PersistenceTask, Owner>,Error> prepareOwner(CreateOwnerCommand cmd, long userId){
+    private Result<Owner, Error> prepareOwner(CreateOwnerCommand cmd, long userId) {
         return this.idGenerator.next()
-                .flatMap(ownerId -> this.commandMapper.toOwner(cmd, ownerId, userId))
-                .flatMap(owner -> this.ownerRepository.prepareSave(owner)
-                        .map(task -> Pair.of(task, owner)));
+                .flatMap(ownerId -> this.commandMapper.toOwner(cmd, ownerId, userId));
+    }
+
+    private Result<OwnerResponse, Error> executeTransaction(User user, Owner owner) {
+        return this.userRepository.prepareSave(user)
+                .flatMap(userTask -> this.ownerRepository.prepareSave(owner)
+                        .flatMap(ownerTask -> this.transaction.write()
+                                .add(userTask)
+                                .add(ownerTask)
+                                .build()
+                                .map(v -> this.responseMapper.toResponse(owner, user))));
     }
 }
