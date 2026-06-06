dfa217857cf1216f836483337520bfddae83963d
Heyther <htrsanbl@gmail.com>
2026-06-05 23:51:22 -0500

    feat: add EmailServiceAutoConfiguration

---
+++ jobby/shared/src/main/java/com/jobby/infrastructure/autoconfiguration/EmailServiceAutoConfiguration.java
@@ -0,0 +1,19 @@
+package com.jobby.infrastructure.autoconfiguration;
+
+import com.jobby.domain.ports.EmailService;
+import com.jobby.infrastructure.adapter.EmailServiceAdapter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Configuration
+@ConditionalOnClass({EmailServiceAutoConfiguration.class})
+public class EmailServiceAutoConfiguration {
+    @Bean
+    @ConditionalOnMissingBean
+    public EmailService getEmailService(JavaMailSender mailSender){
+        return new EmailServiceAdapter(mailSender);
+    }
+}
